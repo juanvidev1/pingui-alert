d9411f4925afd6e7cc4d513154ee53b6a88f84b2
juanvidev <juanvidev1@gmail.com>
2025-11-25 21:44:15 -0500

    feat: add MIT license and update npm build script to copy `LICENSE.md` to the npm package.

--- scripts/build_npm.ts
+++ scripts/build_npm.ts
@@ -25,7 +25,7 @@ await build({
   },
   postBuild() {
     // steps to run after building and before running the tests
-    Deno.copyFileSync("../LICENSE", "npm/LICENSE");
     Deno.copyFileSync("../README.md", "npm/README.md");
+    Deno.copyFileSync("../LICENSE.md", "npm/LICENSE.md");
   },
 });
