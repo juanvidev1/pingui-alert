c62c3690670bb91f8ba9f784b8350be2b6be48e4
juanvidev1 <juanvidev1@gmail.com>
2026-01-31 17:14:53 -0500

    fix: enable database synchronization with alterations in initDB function

--- src/db/init.ts
+++ src/db/init.ts
@@ -3,7 +3,7 @@ import sequelize from './sequelize.js';
 export const initDB = async () => {
   try {
     await sequelize.authenticate();
-    await sequelize.sync();
+    await sequelize.sync({ alter: true });
     console.log('Database connected');
   } catch (error) {
     console.error('Error connecting to database:', error);
